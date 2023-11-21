80634c1fec81c5f3f1076720d39bf6b4f4964839
Bryce Greenfield <brycegreenfield18@gmail.com>
2023-11-20 22:48:23 -0600

    Update higher-lower-summary.js

--- js/higher-lower-summary.js
+++ js/higher-lower-summary.js
@@ -14,16 +14,6 @@ if (urlLvls != null && urlLvls >= 0 && urlLvls <= 3) { levels = urlLvls }
 const urlDiff = getUrlParam('diff')
 if (urlLvls != null && urlDiff >= 0 && urlDiff <= 3) { difficulty = urlDiff }
 
-if (seed != null) {
-    document.getElementById('seed').value = seed
-}
-if (levels != null) {
-    document.getElementById('levels'+levels).checked = true
-}
-if (difficulty != null) {
-    document.getElementById('difficulty'+difficulty).checked = true
-}
-
 console.log(seed)
 console.log(levels)
 console.log(difficulty)
